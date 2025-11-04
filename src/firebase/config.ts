acf97703f4b872fec41b74d5e3363973baa41eff
Fox2099 <gpt.fox2099@gmail.com>
2025-11-04 18:14:00 +0000

    вот этот ключ нужен?

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,10 +3,10 @@
 import { FirebaseOptions } from 'firebase/app';
 
 export const firebaseConfig: FirebaseOptions = {
-  apiKey: "AIzaSyB31n3A5n5f3BvWj9jGg4aY-X-4m8b4V_A",
+  apiKey: "AIzaSyCVho3YI4Q0mLlp5PzeM9mLxwGe77v6-84",
   authDomain: "konk-media-archive.firebaseapp.com",
   projectId: "konk-media-archive",
   storageBucket: "konk-media-archive.appspot.com",
-  messagingSenderId: "351475726916",
-  appId: "1:351475726916:web:c85117462057988391336d"
+  messagingSenderId: "715036389581",
+  appId: "1:715036389581:web:c85117462057988391336d"
 };
