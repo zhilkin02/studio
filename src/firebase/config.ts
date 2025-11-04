ebf1740a6c23e0c31a5db2c1947f62454df21774
Fox2099 <gpt.fox2099@gmail.com>
2025-11-04 18:09:57 +0000

    ты потерял ключ похоже:
    FirebaseError: Firebase: Error (auth/api-key-no

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,10 +3,10 @@
 import { FirebaseOptions } from 'firebase/app';
 
 export const firebaseConfig: FirebaseOptions = {
-  apiKey: 'API_KEY',
-  authDomain: 'konk-media-archive.firebaseapp.com',
-  projectId: 'konk-media-archive',
-  storageBucket: 'konk-media-archive.appspot.com',
-  messagingSenderId: '351475726916',
-  appId: '1:351475726916:web:c85117462057988391336d',
+  apiKey: "API_KEY",
+  authDomain: "konk-media-archive.firebaseapp.com",
+  projectId: "konk-media-archive",
+  storageBucket: "konk-media-archive.appspot.com",
+  messagingSenderId: "351475726916",
+  appId: "1:351475726916:web:c85117462057988391336d"
 };
