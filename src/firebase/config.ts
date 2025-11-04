8e1d4b4ef3a6ab182f6499f35c7490c44142ef6a
Fox2099 <gpt.fox2099@gmail.com>
2025-11-04 10:35:33 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5312529026-9c024",
+  "appId": "1:1098574113497:web:ee21290cbd4c35904eecc8",
+  "apiKey": "AIzaSyD6E-U938BCYdT5dm5QVDGX73eyQkKYT1I",
+  "authDomain": "studio-5312529026-9c024.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1098574113497"
+};
