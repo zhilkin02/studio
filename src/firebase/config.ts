bcfa1df4b5bd0d808ecf966904dda3a7bc17dcff
Fox2099 <gpt.fox2099@gmail.com>
2025-11-04 18:20:08 +0000

    Project name Konk Media Archive Project ID konk-media-archive Project n

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,7 +3,7 @@
 import { FirebaseOptions } from 'firebase/app';
 
 export const firebaseConfig: FirebaseOptions = {
-  apiKey: "AIzaSyCVho3YI4Q0mLlp5PzeM9mLxwGe77v6-84",
+  apiKey: "AIzaSyCVho3YI4Q0mLIp5PzeM9mLxwGe77v6-84",
   authDomain: "konk-media-archive.firebaseapp.com",
   projectId: "konk-media-archive",
   storageBucket: "konk-media-archive.appspot.com",
