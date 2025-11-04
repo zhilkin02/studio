3d5ce24f68cd096be265a2e6ceee2e933c50bcf0
Fox2099 <gpt.fox2099@gmail.com>
2025-11-04 18:11:15 +0000

    Try fixing this error: `Console Error: FirebaseError: Firebase: Error (a

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,7 +3,7 @@
 import { FirebaseOptions } from 'firebase/app';
 
 export const firebaseConfig: FirebaseOptions = {
-  apiKey: "API_KEY",
+  apiKey: "AIzaSyB31n3A5n5f3BvWj9jGg4aY-X-4m8b4V_A",
   authDomain: "konk-media-archive.firebaseapp.com",
   projectId: "konk-media-archive",
   storageBucket: "konk-media-archive.appspot.com",
