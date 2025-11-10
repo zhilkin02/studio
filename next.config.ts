f2dfcfc6e6df904dd5ae639825b37ce3d9f9dfa1
Fox2099 <gpt.fox2099@gmail.com>
2025-11-10 21:50:08 +0000

    всё ли готово для публикации сайта ? он будет работать также как сейчас

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,6 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   images: {
     remotePatterns: [
       {
